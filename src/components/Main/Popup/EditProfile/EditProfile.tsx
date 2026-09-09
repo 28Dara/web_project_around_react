7e5fa365d194fb5024e29788a3d600cd8e32bcb4
Dara <dara.esra28@gmail.com>
2026-09-08 21:10:34 -0600

    fixed a bug on EditProfile

--- src/components/Main/Popup/EditProfile/EditProfile.tsx
+++ src/components/Main/Popup/EditProfile/EditProfile.tsx
@@ -1,10 +1,23 @@
+import { useState, useContext } from 'react';
+import CurrentUserContext from '../../../../contexts/CurrentUserContext';
+
 export default function EditProfile(): React.JSX.Element {
+  const { currentUser, handleUpdateUser } = useContext(CurrentUserContext);
+  const [name, setName] = useState(currentUser?.name || '');
+  const [description, setDescription] = useState(currentUser?.about || '');
+
+  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleUpdateUser({ name, about: description });
+  };
+
   return (
     <form
       className='popup__form'
       id='edit-profile-form'
       name='edit-profile-form'
       noValidate
+      onSubmit={handleSubmit}
     >
       <input
         className='popup__input popup__input_type_name'
@@ -15,6 +28,8 @@ export default function EditProfile(): React.JSX.Element {
         required
         minLength={2}
         maxLength={40}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <span id='name-input-error' className='popup__input-error'></span>
       <input
@@ -26,6 +41,8 @@ export default function EditProfile(): React.JSX.Element {
         required
         minLength={2}
         maxLength={200}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
       <span id='description-input-error' className='popup__input-error'></span>
       <button className='button popup__button' type='submit'>
